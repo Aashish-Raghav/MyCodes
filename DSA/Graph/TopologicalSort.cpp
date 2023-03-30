6fb715ae5dc9fb7d147024b97dc1c4e1000cc4d0
Aashish <aashishraghav627@gmail.com>
2023-03-30 22:29:49 +0530

    Topological Sort using BFS

--- DSA/Graph/TopologicalSort.cpp
+++ DSA/Graph/TopologicalSort.cpp
@@ -12,8 +12,6 @@ void dfs(unordered_map<int,list<int>> &adjList,vector<bool> &visited,
     }
     ans.push_back(node);
 }
-
-
 vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
     unordered_map<int,list<int>> adjList;
     for (auto i : edges){
@@ -32,5 +30,44 @@ vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
 
     //rather than this can use stack and copy all elements to vector
     reverse(ans.begin(),ans.end());
+    return ans;
+}
+
+
+
+
+//using BFS
+vector<int> topologicalSort2(vector<vector<int>> &edges, int v, int e)  {
+    unordered_map<int,list<int>> adjList;
+    
+    for (int i = 0;i < edges.size();i++){
+        int u = edges[i][0];
+        int v = edges[i][1];
+        adjList[u].push_back(v);
+    }
+    vector<int> indegree(v,0);
+    for (auto i : adjList){
+        for (int j : i.second){
+            indegree[j]++;
+        }
+    }
+    queue<int> q;
+    for (int i = 0;i < v;i++){
+        if (indegree[i] == 0)
+            q.push(i);
+    }
+    vector<int> ans;
+    while (!q.empty()){
+        int value = q.front();
+        q.pop();
+        ans.push_back(value);
+
+        for (auto neighbour : adjList[value]){
+            indegree[neighbour]--;
+            if (indegree[neighbour] == 0)
+                q.push(neighbour);
+        }
+    }
+
     return ans;
 }
