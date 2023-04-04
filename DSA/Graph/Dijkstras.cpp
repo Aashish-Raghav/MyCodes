633d19b7b7b0c13caca0c85bac41b7aa65241488
Aashish <aashishraghav627@gmail.com>
2023-04-04 15:21:10 +0530

    Dijkstra's Algorithm

---
+++ DSA/Graph/Dijkstras.cpp
@@ -0,0 +1,43 @@
+//Dijktras algorithm
+#include <bits/stdc++.h> 
+using namespace std;
+
+vector<int> dijkstra(vector<vector<int>> &vec, int vertices, int edges, int source) {
+    unordered_map<int,list<pair<int,int>>> adjList;
+    for (auto i : vec){
+        int u = i[0];
+        int v = i[1];
+        int weight = i[2];
+
+        adjList[u].push_back({v,weight});
+        adjList[v].push_back({u,weight});
+    }
+
+
+    set<pair<int,int>> s;
+    s.insert({0,source});
+    vector<int> dist(vertices,INT_MAX);
+    dist[source] = 0;
+
+    while (!s.empty()){
+        pair<int,int> top = *(s.begin());
+        int nodeDist = top.first;
+        int topNode = top.second;
+        s.erase(top);
+
+        for (auto i : adjList[topNode]){
+            if (dist[i.first] > nodeDist + i.second){
+                pair<int,int> record = {dist[i.first],topNode};
+                
+                //if record present erase it;
+                if (s.find(record) != s.end()){
+                    s.erase(record);
+                }
+                dist[i.first] = nodeDist + i.second;
+                // cout << ans[i.first] << " ";
+                s.insert({dist[i.first],i.first});
+            }
+        }
+    }
+    return dist;
+}
