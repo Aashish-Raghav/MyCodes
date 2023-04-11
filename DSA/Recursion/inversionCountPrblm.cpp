3ce730b2229b50771a3c8acfae1a9d4cc526b3a3
Aashish <aashishraghav627@gmail.com>
2023-04-11 19:29:26 +0530

    inversion count

--- DSA/Recursion/inversionCountPrblm.cpp
+++ DSA/Recursion/inversionCountPrblm.cpp
@@ -1,72 +1,56 @@
 #include <iostream>
 using namespace std;
 
-int merge(int* arr,int start,int mid,int end)
-{
-    int len1 = mid - start + 1;
-    int len2 = end - mid;
-
-    //creating temporary array;
-    int* first = new int[len1];
-    int* second = new int[len2];
-
-    //copying the values;
-    int arrayIndex = start;
-    for (int i = 0;i < len1;i++)
-        first[i] = arr[arrayIndex++];
-    
-    arrayIndex = mid + 1;
-    for (int i = 0;i < len2;i++)
-        second[i] = arr[arrayIndex++];
-
-    int i = 0,j = 0;
-    arrayIndex = start;
-    int temp = start;
-    int ans = 0;
-    while (i < len1 && j < len2)
-    {
-        if (second[j] < first[i])
-        {
-            arr[arrayIndex++] = second[j++];
-            ans += mid - temp;
+long long merge(long long arr[],long long start,long long mid,long long end
+        ,long long temp[]){
+        long long inver_count = 0;
+        long long i = start;
+        long long j = mid;
+        long long k = start;
+        while (i <= mid-1 && j <= end){
+            if (arr[i] <= arr[j]){
+                temp[k++] = arr[i++];
+            }
+            else{
+                temp[k++] = arr[j++];
+                
+                inver_count += mid - i;
+            }           
         }
-        else{
-            arr[arrayIndex++] = first[i++]; temp++;
+        
+        //remaining elements;
+        while (i <= mid-1){
+            temp[k++] = arr[i++];
         }
-    }
-
-    while (i < len1)
-    {
-        arr[arrayIndex++] = first[i++];
-    }
+        
+        while (j <= end){
+            temp[k++] = arr[j++];
+        }
+        
+        //copying merged elements to original array;
+        for (i = start;i <= end;i++){
+            arr[i] = temp[i];
+        }
+        
+        return inver_count;
+    }  
     
-    while (j < len2)
-        arr[arrayIndex++] = second[j++];
-    cout << "ans " << ans << endl;
-    return ans;
-}
-
-int mergeSort(int* arr,int start,int end)
-{
-    int ans = 0,mid;
-    //base case;
-    if (start < end)
-    {
-        mid = start + (end - start)/2;
-
-
-        ans += mergeSort(arr,start,mid);
-        // cout << "ans 2 " << ans << endl;        
-        ans += mergeSort(arr,mid+1,end);
-        // cout << "ans 3 " << ans << endl;        
-        ans += merge(arr,start,mid,end);
-        cout << "ans 4 " << ans << endl;
+    
+    long long mergeSort(long long arr[],long long start,long long end,long long temp[]){
+        long long inver_count = 0;
+        if (start < end){
+            long long mid = start + (end - start)/2;
+            inver_count += mergeSort(arr,start,mid,temp);
+            inver_count += mergeSort(arr,mid+1,end,temp);
+            
+            inver_count += merge(arr,start,mid+1,end,temp);
+        }
+        
+        return inver_count;
     }
-    return ans;
-}
-
-int main()
-{
-    int arr[] = {8,4,2,1};
-    cout << mergeSort(arr,0,3) << endl;
-}
+    long long int inversionCount(long long arr[], long long N)
+    {
+        long long start = 0,end = N-1;
+        long long temp[N];
+        return mergeSort(arr,start,end,temp);
+    }
