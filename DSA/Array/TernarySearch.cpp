3b2197e2b1528e2b03baa8637d9aa18f5ffaa584
Aashish <aashishraghav627@gmail.com>
2023-04-18 21:59:44 +0530

    Ternary Search

--- DSA/Array/TernarySearch.cpp
+++ DSA/Array/TernarySearch.cpp
@@ -2,6 +2,8 @@
 using namespace std;
 
 int ternarySearch(int arr[],int l,int r,int key){
+    if (l > r)
+        return -1;
     int mid1 = l + (r-l)/3;
     int mid2 = r - (r-l)/3;
     
@@ -18,9 +20,12 @@ int ternarySearch(int arr[],int l,int r,int key){
         return ternarySearch(arr,mid2+1,r,key);
     }
     else
-        return ternarySearch(arr,mid+1,mid2 - 1,key);
+        return ternarySearch(arr,mid1+1,mid2 - 1,key);
 }
 
 int main(){
-
+    int arr[10] = {10,21,30,46,54,60,71,87,945,1000};
+    int key = 1000;
+    int index = ternarySearch(arr,0,9,key);
+    cout << key << " at " << index << endl;
 }
